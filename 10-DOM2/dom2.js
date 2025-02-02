68337ce0d9e8693e5d60e1729d40e176d8ab8dca
Milan2501 <milanvasovic06@gmail.com>
2025-02-03 00:56:35 +0100

    Ispisao odredjene vjezbe

---
+++ 10-DOM2/dom2.js
@@ -0,0 +1,254 @@
+let container = document.querySelector(".estate");
+let citiesSelector = document.querySelector("#cities");
+let optionsSelector = document.querySelector("#options");
+let typesSelector = document.querySelector("#types");
+let search = document.querySelector("#search");
+
+let citiesArray = [
+    "Belgrade", 
+    "Zagreb", 
+    "Ljubljana", 
+    "Sarajevo", 
+    "Skopje", 
+    "Podgorica", 
+    "Novi Sad", 
+    "Split", 
+    "Banja Luka", 
+    "Maribor"
+  ];
+let optionsArray = [
+    "stan",
+    "plac",
+    "garaza",
+    "kuca"
+];
+let apartmentsTypes = [
+    "garsonjera",
+    "jednosoban",
+    "dvosoban",
+    "trosoban",
+    "cetvorosoban"
+];
+
+let arrOfObjects = [
+    {
+        city: "Belgrade",
+        option: "stan",
+        type: "garsonjera",
+        size: 34,
+        price: 77000
+    },
+    {
+        city: "Zagreb",
+        option: "kuca",
+        type: "trosoban",
+        size: 120,
+        price: 150000
+    },
+    {
+        city: "Ljubljana",
+        option: "stan",
+        type: "jednosoban",
+        size: 45,
+        price: 85000
+    },
+    {
+        city: "Sarajevo",
+        option: "plac",
+        type: null,
+        size: 1000,
+        price: 60000
+    },
+    {
+        city: "Skopje",
+        option: "stan",
+        type: "cetvorosoban",
+        size: 90,
+        price: 130000
+    },
+    {
+        city: "Podgorica",
+        option: "garaza",
+        type: null,
+        size: 20,
+        price: 25000
+    },
+    {
+        city: "Novi Sad",
+        option: "stan",
+        type: "dvosoban",
+        size: 55,
+        price: 95000
+    },
+    {
+        city: "Split",
+        option: "kuca",
+        type: "cetvorosoban",
+        size: 150,
+        price: 180000
+    },
+    {
+        city: "Banja Luka",
+        option: "stan",
+        type: "trosoban",
+        size: 75,
+        price: 110000
+    },
+    {
+        city: "Maribor",
+        option: "plac",
+        type: null,
+        size: 850,
+        price: 70000
+    }
+];
+
+
+for (let city of citiesArray) {
+    let optCity = document.createElement("option");
+    optCity.textContent = city;
+    optCity.value = city.toLowerCase();
+    citiesSelector.appendChild(optCity);
+}
+
+for (let pick of optionsArray) {
+    let optOptions = document.createElement("option");
+    optOptions.textContent = pick;
+    optOptions.value = pick.toLowerCase();
+    optionsSelector.appendChild(optOptions); 
+}
+
+for (let type of apartmentsTypes) {
+    let optTypes = document.createElement("option");
+    optTypes.textContent = type;
+    optTypes.value = type.toLowerCase();
+    typesSelector.appendChild(optTypes);
+}
+
+let currentCity = null;
+let currentOptions = null;
+let currentTypes = null;
+
+citiesSelector.addEventListener("change", function(e) {
+    currentCity = e.currentTarget.value;
+})
+
+optionsSelector.addEventListener("change", function(e) {
+    currentOptions = e.currentTarget.value;
+})
+
+typesSelector.addEventListener("change", function(e) {
+    currentTypes = e.currentTarget.value;
+})
+
+
+let realEstate = document.querySelector(".realEstate");
+
+for (let el of arrOfObjects) {
+    let obj = document.createElement("div");
+    let title = document.createElement("h4");
+    title.classList.add("city")
+    title.textContent = el.city + " " + el.option;
+    let types = document.createElement("p");
+    types.textContent = el.type;
+    let sizes = document.createElement("p");
+    sizes.textContent = el.size;
+    let prices = document.createElement("p");
+    prices.textContent = el.price;
+    let separateLine = document.createElement("p");
+    separateLine.textContent = "-----------"
+    obj.append(title, types, sizes, prices, separateLine);
+    realEstate.append(obj);  
+}
+
+console.log(realEstate.querySelector(".obj"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
