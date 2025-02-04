e64f73c3c2932873cc27dffc7688bf7552b342b9
Milan2501 <milanvasovic06@gmail.com>
2025-02-04 19:22:36 +0100

    Dodao domaci

---
+++ 11-Functions/fun.js
@@ -0,0 +1,82 @@
+
+function login(userName, password) {
+    if (userName === "admin" && password === "admin") {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(login("admin", "admin"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
