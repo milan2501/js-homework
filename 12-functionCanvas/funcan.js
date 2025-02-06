5f268b1d4a9f66439ca1d9b649ad5aca782268d4
Milan2501 <milanvasovic06@gmail.com>
2025-02-06 09:33:35 +0100

    Dodao domaci

--- 12-functionCanvas/funcan.js
+++ 12-functionCanvas/funcan.js
@@ -3,40 +3,32 @@ let ctx = canvas.getContext("2d");
 let addBtn = document.querySelector("#add");
 let resetBtn = document.querySelector("#reset");
 
-// let array = [];
-// canvas.addEventListener("click", function (e) {
-//     let rect = canvas.getBoundingClientRect();
-//     let x = e.clientX - rect.left;
-//     let y = e.clientY - rect.top;
-//     array.push({ x: x, y: y })
-//     for (let i = 0; i < array.length - 1; i++) {
-//         ctx.beginPath();
-//         ctx.moveTo(array[i].x, array[i].y);
-//         ctx.lineTo(array[i + 1].x, array[i + 1].y);
-//         ctx.stroke();
-//     }
-// }
-// );
 
-addBtn.addEventListener("click", function () {
-    let array = [];
-    canvas.addEventListener("click", function (e) {
-        let rect = canvas.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-        array.push({ x: x, y: y })
-        for (let i = 0; i < array.length - 1; i++) {
-            ctx.beginPath();
-            ctx.moveTo(array[i].x, array[i].y);
-            ctx.lineTo(array[i + 1].x, array[i + 1].y);
+
+let array = [];
+canvas.addEventListener("click", function (e) {
+    let rect = canvas.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    array.push({ x: x, y: y });
+    if (array.length >= 5) {
+        ctx.beginPath();
+        ctx.moveTo(array[0].x, array[0].y);
+    
+        for (let i in array) {
+            ctx.lineTo(array[i].x, array[i].y);
+        };
+        addBtn.addEventListener("click", function() {
             ctx.stroke();
-        }
+        })
+        array = [];
     }
-    );
-})
+    resetBtn.addEventListener("click", function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    })
 
-
-
+}
+);
 
 
 
