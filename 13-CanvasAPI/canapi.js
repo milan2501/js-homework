f5bf763eccddc7abba029df8d41974270c92fe92
Milan2501 <milanvasovic06@gmail.com>
2025-02-10 15:31:53 +0100

    Dodao radove i vezbe iz lekcije

--- 13-CanvasAPI/canapi.js
+++ 13-CanvasAPI/canapi.js
@@ -3,9 +3,11 @@ let canvas = document.querySelector("#drawArea");
 let ctx = canvas.getContext("2d");
 let addBtn = document.querySelector("#add");
 let resetBtn = document.querySelector("#reset");
+let container = document.querySelector("#container");
+let nameOfRandomColor = document.createElement("p");
+let colorBtn = document.querySelector("#color");
 
 
-let colors = ["red", "green", "blue", "yellow"];
 let array = [];
 canvas.addEventListener("click", function (e) {
     let rect = canvas.getBoundingClientRect();
@@ -13,58 +15,49 @@ canvas.addEventListener("click", function (e) {
     let y = e.clientY - rect.top;
     array.push({ x: x, y: y });
     if (array.length >= 5) {
-        
-        //printing of coordinates
-        ctx.beginPath();
-        ctx.moveTo(array[0].x, array[0].y);
-      
-        for (let i in array) {
-            ctx.lineTo(array[i].x, array[i].y);
-        };
-        
-        //getting of random colors and adding draw
-        addBtn.addEventListener("click", function() {
-            for (let j = 0; j < colors.length; j++) {
-                let randomColor = Math.floor(Math.random()  * colors.length);
-                ctx.strokeStyle = colors[randomColor];
-                ctx.lineWidth = 2;
-            }
-            ctx.stroke();
-        })
-        array = [];
+    //drawing button  
+    colorBtn.addEventListener("click", paintLine);
+    drawPath(chosenColor)
+    addBtn.addEventListener("click", printColor);
+    array = [];
     }
-    
-    
     //delete button
-    resetBtn.addEventListener("click", function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    })
-
-}
+    resetBtn.addEventListener("click", clearCanvas);
+    }
 );
 
+//drawing funciton
+function drawPath(color) {
+    ctx.beginPath();
+    ctx.moveTo(array[0].x, array[0].y);
+    for (let i in array) {
+         ctx.lineTo(array[i].x, array[i].y);
+    };
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;  
+}
 
+//printing color
+function printColor() {
+    let randomColorForPrint = chosenColor;
+    ctx.stroke();
+    nameOfRandomColor.classList.add("printedColor");
+    nameOfRandomColor.textContent = `The color of the line is ${randomColorForPrint}`;
+    container.appendChild(nameOfRandomColor);
+}
 
+//clearing canvas
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    nameOfRandomColor.innerText = "";
+    container.appendChild(nameOfRandomColor);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let chosenColor = null;
+function paintLine() {
+    chosenColor = prompt("Enter color:");
+    return chosenColor;
+}
 
 
 
