bf33f1aabf07b8dc149b9343deaf80c44896b3ee
Milan2501 <milanvasovic06@gmail.com>
2025-02-13 15:51:17 +0100

    Dodao domaci

--- 14-Fetch/fetch.js
+++ 14-Fetch/fetch.js
@@ -1,8 +1,57 @@
-fetch("https://dummyjson.com/products/1", { method: "HEAD" })
-    .then(response => {
-        console.log("Headers:", response.headers);
-        console.log("Status:", response.status);
-    })
-    .catch(error => console.error("Error:", error));
+
+let input = document.createElement('input');
+document.body.append(input);
+let addBtn = document.createElement('button');
+addBtn.innerText = 'Search';
+addBtn.style.margin = '10px'
+document.body.append(addBtn);
+addBtn.addEventListener("click", function() {
+    let add = input.value;
+    fetch(`https://dummyjson.com/products/search?q=${add}`)
+    .then(res => res.json())
+    .then(console.log)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
